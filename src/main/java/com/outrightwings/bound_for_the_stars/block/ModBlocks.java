44823fa674f4678eaf80f38bdc3246094cec36e7
OutrightWings <lilymason@msn.com>
2025-09-17 13:18:48 -0700

    Lots of moon blocks/data files

--- src/main/java/com/outrightwings/bound_for_the_stars/block/ModBlocks.java
+++ src/main/java/com/outrightwings/bound_for_the_stars/block/ModBlocks.java
@@ -20,6 +20,11 @@ public class ModBlocks {
         public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
         public static final Tuple<RegistryObject<? extends Block>, RegistryObject<BlockItem>> TELESCOPE = createBlockWItem("telescope",()->new TelescopeBlock(BlockBehaviour.Properties.copy(Blocks.SMITHING_TABLE)));
         public static final Tuple<RegistryObject<? extends Block>, RegistryObject<BlockItem>> MOON_TERRACOTTA = createBlockWItem("moon_terracotta",()->new GlazedTerracottaBlock(BlockBehaviour.Properties.copy(Blocks.STONE)));
+        public static final Tuple<RegistryObject<? extends Block>, RegistryObject<BlockItem>> MOON_DUST = createBlockWItem("moon_dust",() -> new GravelBlock(BlockBehaviour.Properties.copy(Blocks.GRAVEL)));
+        public static final Tuple<RegistryObject<? extends Block>, RegistryObject<BlockItem>> MOONSTONE_PILLAR = createBlockWItem("moonstone_pillar",() -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STONE)));
+        public static final Tuple<RegistryObject<? extends Block>, RegistryObject<BlockItem>> MOONSTONE_TERRACOTTA = createBlockWItem("moonstone_terracotta",()->new GlazedTerracottaBlock(BlockBehaviour.Properties.copy(Blocks.STONE)));
+        public static final Tuple<RegistryObject<? extends Block>, RegistryObject<BlockItem>> MOON_DUST_ROCKY = createBlockWItem("moon_dust_rocky",() -> new GravelBlock(BlockBehaviour.Properties.copy(Blocks.GRAVEL)));
+        public static final Tuple<RegistryObject<? extends Block>, RegistryObject<BlockItem>> MOONSTONE_PILLAR_END = createBlockWItem("moonstone_pillar_end",() -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STONE)));
 
         //Block sets
         public static final List<Tuple<RegistryObject<? extends Block>,RegistryObject<BlockItem>>> BLACK_COBBLE = createBaseBlockSet("black_cobble");
@@ -27,6 +32,12 @@ public class ModBlocks {
         public static final List<Tuple<RegistryObject<? extends Block>,RegistryObject<BlockItem>>> MOON_ROCK = createBaseBlockSet("moon_rock");
         public static final List<Tuple<RegistryObject<? extends Block>,RegistryObject<BlockItem>>> RED_COBBLE = createBaseBlockSet("red_cobble");
         public static final List<Tuple<RegistryObject<? extends Block>,RegistryObject<BlockItem>>> RED_ROCK = createBaseBlockSet("red_rock");
+        public static final List<Tuple<RegistryObject<? extends Block>,RegistryObject<BlockItem>>> CHEESE = createBaseBlockSet("cheese");
+        public static final List<Tuple<RegistryObject<? extends Block>,RegistryObject<BlockItem>>> MOON_COBBLE = createBaseBlockSet("moon_cobble");
+        public static final List<Tuple<RegistryObject<? extends Block>,RegistryObject<BlockItem>>> MOONSTONE = createBaseBlockSet("moonstone");
+        public static final List<Tuple<RegistryObject<? extends Block>,RegistryObject<BlockItem>>> PACKED_MOON_DUST = createBaseBlockSet("packed_moon_dust");
+
+
 
         private static List<Tuple<RegistryObject<? extends Block>, RegistryObject<BlockItem>>> createBaseBlockSet(String name){
                 List<Tuple<RegistryObject<? extends Block>,RegistryObject<BlockItem>>> blockSet = new ArrayList<>();
