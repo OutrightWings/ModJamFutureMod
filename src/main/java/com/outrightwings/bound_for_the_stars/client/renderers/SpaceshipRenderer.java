5b75de05649b18bba227b9beb449df20789aacc1
OutrightWings <lilymason@msn.com>
2025-09-21 13:16:18 -0700

    Spaceship translucent texture

--- src/main/java/com/outrightwings/bound_for_the_stars/client/renderers/SpaceshipRenderer.java
+++ src/main/java/com/outrightwings/bound_for_the_stars/client/renderers/SpaceshipRenderer.java
@@ -2,18 +2,28 @@ package com.outrightwings.bound_for_the_stars.client.renderers;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
+import com.outrightwings.bound_for_the_stars.Main;
 import com.outrightwings.bound_for_the_stars.client.models.SpaceshipGeo;
 import com.outrightwings.bound_for_the_stars.entity.Spaceship;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 @OnlyIn(Dist.CLIENT)
 public class SpaceshipRenderer extends GeoEntityRenderer<Spaceship> {
-
+    private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(Main.MODID, "textures/entity/spaceship.png");
     public SpaceshipRenderer(EntityRendererProvider.Context context) {
         super(context, new SpaceshipGeo());
+    }
+
+    @Override
+    public RenderType getRenderType(Spaceship animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
+        return RenderType.entityTranslucent(texture);
     }
 
     @Override
