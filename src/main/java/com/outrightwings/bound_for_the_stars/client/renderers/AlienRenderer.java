6238829b607599f7b1b5b926ca54f482d7c86ee3
OutrightWings <lilymason@msn.com>
2025-10-06 15:10:24 -0700

    Cute alien start

--- src/main/java/com/outrightwings/bound_for_the_stars/client/renderers/AlienRenderer.java
+++ src/main/java/com/outrightwings/bound_for_the_stars/client/renderers/AlienRenderer.java
@@ -1,14 +1,52 @@
 package com.outrightwings.bound_for_the_stars.client.renderers;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.outrightwings.bound_for_the_stars.Main;
 import com.outrightwings.bound_for_the_stars.client.models.AlienGeo;
 import com.outrightwings.bound_for_the_stars.client.models.SpaceshipGeo;
 import com.outrightwings.bound_for_the_stars.entity.Alien;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.cache.object.GeoBone;
+import software.bernie.geckolib.loading.json.raw.Bone;
 import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class AlienRenderer extends GeoEntityRenderer<Alien> {
     public AlienRenderer(EntityRendererProvider.Context context) {
         super(context, new AlienGeo());
+    }
+    public ResourceLocation getTextureLocation(Alien entity) {
+        int color = entity.skinColor();
+        return switch (color) {
+            case 1 -> ResourceLocation.fromNamespaceAndPath(Main.MODID, "textures/entity/alien2.png");
+            case 2 -> ResourceLocation.fromNamespaceAndPath(Main.MODID, "textures/entity/alien1.png");
+            default -> ResourceLocation.fromNamespaceAndPath(Main.MODID, "textures/entity/alien.png");
+        };
+    }
+    public void renderRecursively(PoseStack poseStack, Alien alien, GeoBone bone, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+
+        // cape
+        if (bone.getName().equals("cape")) {
+            if (!alien.hasCape()) {
+                bone.setHidden(true);
+            } else {
+                bone.setHidden(false);
+            }
+        }
+
+        // antena
+        if (bone.getName().startsWith("antena")) {
+            int count = alien.antennaCount();
+            bone.setHidden(false);
+            if(count == 0) bone.setHidden(true);
+            if(count == 1 && bone.getName().startsWith("antenaA")) bone.setHidden(true);
+            if(count == 2 && bone.getName().startsWith("antenaB")) bone.setHidden(true);
+        }
+
+        super.renderRecursively(poseStack,alien,bone,renderType,bufferSource,buffer,isReRender,partialTick,packedLight,packedOverlay,red,green,blue,alpha);
     }
 }
--- src/main/java/com/outrightwings/bound_for_the_stars/entity/Alien.java
+++ src/main/java/com/outrightwings/bound_for_the_stars/entity/Alien.java
@@ -1,17 +1,22 @@
 package com.outrightwings.bound_for_the_stars.entity;
 
 import com.outrightwings.bound_for_the_stars.Main;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
@@ -21,13 +26,64 @@ import software.bernie.geckolib.core.animation.RawAnimation;
 import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
+import javax.annotation.Nullable;
+
 public class Alien extends PathfinderMob implements GeoEntity {
+    private static final EntityDataAccessor<Boolean> HAS_CAPE = SynchedEntityData.defineId(Alien.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Integer> ANTENNAS = SynchedEntityData.defineId(Alien.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Integer> SKIN_COLOR = SynchedEntityData.defineId(Alien.class, EntityDataSerializers.INT);
+
     protected Alien(EntityType<? extends PathfinderMob> type, Level level) {
         super(type, level);
     }
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, (double)10.0F).add(Attributes.MOVEMENT_SPEED, (double)0.23F);
     }
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(HAS_CAPE, false);
+        this.entityData.define(ANTENNAS, 0);
+        this.entityData.define(SKIN_COLOR, 0);
+    }
+    @Override
+    public void addAdditionalSaveData(CompoundTag tag) {
+        super.addAdditionalSaveData(tag);
+        tag.putBoolean("cape", hasCape());
+        tag.putInt("antenna", antennaCount());
+        tag.putInt("color", skinColor());
+    }
+    @Override
+    public void readAdditionalSaveData(CompoundTag tag) {
+        super.readAdditionalSaveData(tag);
+        if (tag.contains("cape")) {
+            this.entityData.set(HAS_CAPE, tag.getBoolean("cape"));
+        }
+        if (tag.contains("antenna")) {
+            this.entityData.set(ANTENNAS, tag.getInt("antenna"));
+        }
+        if (tag.contains("color")) {
+            this.entityData.set(SKIN_COLOR, tag.getInt("color"));
+        }
+    }
+
+    public int antennaCount(){return this.entityData.get(ANTENNAS);}
+    public int skinColor(){return this.entityData.get(SKIN_COLOR);}
+    public boolean hasCape(){return this.entityData.get(HAS_CAPE);}
+    @Override
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType reason, @Nullable SpawnGroupData data, @Nullable CompoundTag tag) {
+        data = super.finalizeSpawn(level, difficulty, reason, data, tag);
+
+        this.entityData.set(HAS_CAPE, this.random.nextFloat() < 0.1f); // 10% chance
+        this.entityData.set(ANTENNAS, this.random.nextInt(4)); // 0–3
+        this.entityData.set(SKIN_COLOR, this.random.nextInt(3)); // 0–2, three colors
+
+        if (this.random.nextFloat() < 0.25f) { // 25% chance for sword
+            this.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.IRON_SWORD));
+        }
+
+        return data;
+    }
+
     protected void registerGoals() {
         //this.eatBlockGoal = new EatBlockGoal(this);
         this.goalSelector.addGoal(0, new FloatGoal(this));
@@ -44,10 +100,10 @@ public class Alien extends PathfinderMob implements GeoEntity {
     protected static final RawAnimation IDLE_ANIM = RawAnimation.begin().thenLoop("alien.idle");
     protected static final RawAnimation WALK_ANIM = RawAnimation.begin().thenLoop("alien.walk");
     protected static final RawAnimation RUN_ANIM = RawAnimation.begin().thenLoop("alien.run");
-    protected static final RawAnimation IDLE_ANIM2 = RawAnimation.begin().thenLoop("alien.idle2");
-    protected static final RawAnimation WALK_ANIM2 = RawAnimation.begin().thenLoop("alien.walk2");
-    protected static final RawAnimation RUN_ANIM2 = RawAnimation.begin().thenLoop("alien.run2");
-    protected static final RawAnimation SHOOT_ANIM = RawAnimation.begin().thenLoop("alien.shoot");
+    protected static final RawAnimation IDLE_ANIM2 = RawAnimation.begin().thenLoop("alien.idleGun");
+    protected static final RawAnimation WALK_ANIM2 = RawAnimation.begin().thenLoop("alien.walkGun");
+    protected static final RawAnimation RUN_ANIM2 = RawAnimation.begin().thenLoop("alien.runGun");
+    //protected static final RawAnimation SHOOT_ANIM = RawAnimation.begin().thenLoop("alien.shoot");
 
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return geoCache;
@@ -65,6 +121,6 @@ public class Alien extends PathfinderMob implements GeoEntity {
                 return event.setAndContinue(WALK_ANIM2);
             }
         }
-        return event.setAndContinue(IDLE_ANIM);
+        return event.setAndContinue(IDLE_ANIM2);
     }
 }
